0819bd7e6d633d1633072a6bea44ba335fea2ec8
Dennis Peter <peterdennis573@gmail.com>
2025-04-15 14:55:29 +0300

    Updated the authentication slice

--- client/src/store/slices/authSlice.ts
+++ client/src/store/slices/authSlice.ts
@@ -4,6 +4,7 @@ interface AuthState {
   isAuthenticated: boolean;
   token: string | null;
   user: any;
+  amount_invested: number | null;
 }
 
 const initialState: AuthState = {
@@ -11,6 +12,9 @@ const initialState: AuthState = {
   token: sessionStorage.getItem("authToken"),
   user: sessionStorage.getItem("user")
     ? JSON.parse(sessionStorage.getItem("user")!)
+    : null,
+  amount_invested: sessionStorage.getItem("amount_invested")
+    ? JSON.parse(sessionStorage.getItem("amount_invested")!)
     : null,
 };
 
@@ -23,35 +27,66 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.isAuthenticated = true;
 
+      state.amount_invested = Number(action.payload.user.investment_amount);
+
+      sessionStorage.setItem(
+        "amount_invested",
+        JSON.stringify(action.payload.user.investment_amount)
+      );
       sessionStorage.setItem("authToken", action.payload.token);
       sessionStorage.setItem("user", JSON.stringify(action.payload.user));
     },
+
     logout(state) {
       state.token = null;
       state.isAuthenticated = false;
       state.user = null;
+      state.amount_invested = null;
       sessionStorage.removeItem("authToken");
       sessionStorage.removeItem("user");
+      sessionStorage.removeItem("amount_invested");
     },
-    decrementInvestment(state, action: PayloadAction<number>) {
-      const currentAmount = parseFloat(state.user.investment_amount) || 0;
-      const deduction = action.payload;
 
-      const updatedInvestment = Math.max(0, currentAmount - deduction);
+    decrementInvestment(state, action: PayloadAction<{ amount: number }>) {
+      if (state.amount_invested !== null) {
+        state.amount_invested = Math.max(
+          0,
+          state.amount_invested - action.payload.amount
+        );
+        sessionStorage.setItem(
+          "amount_invested",
+          JSON.stringify(state.amount_invested)
+        );
 
-      const newUser = {
-        ...state.user,
-        investment_amount: updatedInvestment.toString(),
-      };
+        if (state.user) {
+          state.user.investment_amount = state.amount_invested.toString();
+          sessionStorage.setItem("user", JSON.stringify(state.user));
+        }
+      }
+    },
 
-      // Update session storage
-      sessionStorage.setItem("user", JSON.stringify(newUser));
+    incrementInvestment(state, action: PayloadAction<{ amount: number }>) {
+      if (state.amount_invested !== null) {
+        state.amount_invested += action.payload.amount;
+        sessionStorage.setItem(
+          "amount_invested",
+          JSON.stringify(state.amount_invested)
+        );
 
-      // Update Redux state
-      state.user = newUser;
+        if (state.user) {
+          state.user.investment_amount = state.amount_invested.toString();
+          sessionStorage.setItem("user", JSON.stringify(state.user));
+        }
+      }
     },
   },
 });
 
-export const { login, logout, decrementInvestment } = authSlice.actions;
+export const {
+  login,
+  logout,
+  decrementInvestment,
+  incrementInvestment,
+} = authSlice.actions;
+
 export default authSlice;
